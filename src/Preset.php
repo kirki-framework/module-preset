e3e670341e4586e3f5dba89382ba58b860044314
Ari Stathopoulos <aristath@gmail.com>
2019-09-22 17:08:07 +0300

    migrate preset to new structure

--- src/Preset.php
+++ src/Preset.php
@@ -13,15 +13,20 @@ namespace Kirki\Module;
 
 use Kirki\URL;
 
-// Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
-
 /**
  * Adds styles to the customizer.
  */
 class Preset {
+
+	/**
+	 * An array of preset controls and their arguments.
+	 *
+	 * @static
+	 * @access private
+	 * @since 1.0.0
+	 * @var array
+	 */
+	private static $preset_controls = [];
 
 	/**
 	 * Constructor.
@@ -31,6 +36,22 @@ class Preset {
 	 */
 	public function __construct() {
 		add_action( 'customize_controls_print_footer_scripts', [ $this, 'customize_controls_print_footer_scripts' ] );
+		add_filter( 'kirki_field_add_control_args', [ $this, 'field_add_control_args' ] );
+	}
+
+	/**
+	 * Filter control arguments.
+	 *
+	 * @access public
+	 * @since 1.0
+	 * @param array $args The field arguments.
+	 * @return array
+	 */
+	public function field_add_control_args( $args ) {
+		if ( isset( $args['preset'] ) && is_array( $args['preset'] ) ) {
+			self::$preset_controls[ $args['settings'] ] = $args['preset'];
+		}
+		return $args;
 	}
 
 	/**
@@ -41,5 +62,6 @@ class Preset {
 	 */
 	public function customize_controls_print_footer_scripts() {
 		wp_enqueue_script( 'kirki-preset', URL::get_from_path( __DIR__ . '/script.js' ), [ 'jquery' ], '1.0.0', false );
+		wp_localize_script( 'kirki-preset', 'kirkiPresetControls', self::$preset_controls );
 	}
 }
